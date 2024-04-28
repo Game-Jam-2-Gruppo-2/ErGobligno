0b8ec93d5b5857f4ebbc4d6739f7672436d75a86
alessio picoco <alessio.picoco@gmail.com>
2024-04-28 20:23:06 +0200

    pretty much did nothing :)
    sorry flavio

--- Assets/Code/Scripts/Movement_Scripts/MovementController.cs
+++ Assets/Code/Scripts/Movement_Scripts/MovementController.cs
@@ -1,27 +1,73 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+	[Header("Serialize:")]
 	[SerializeField] public Rigidbody Rb;
-	[SerializeField] public float MaxSpeed, AccelerationSpeed, DecelerationSpeed;
-	[SerializeField] public float MaxJumpHight;
 	[SerializeField] public LayerMask ClimableLayers;
+
+	[Header("Max Speed Settings: ")]
+	[SerializeField] public float MaxSpeed;
+	[SerializeField] public float MaxJumpHight;
+
+	#region Momentum Settings:
+	[Header("Momentum Settings:")]
+
+	#region Acceleration:
 	[SerializeField] public AnimationCurve AccelerationCurve; // how fast you XLR8
 
+	[Tooltip("how fast you Accelerate")]
+	[SerializeField] public float AccelerationSpeed;
+	#endregion
+
+	#region Deceleration:
 	[Tooltip("how you stop moving based on decelerationSpeed")]
 	[SerializeField] public AnimationCurve DecelerationCurve;
 
-	[HideInInspector] public float MomentumCounter; // whare you are inside one of the curves
-	[SerializeField] public MovementStates CurrentState;
+	[Tooltip("how fast you decelerate over time")]
+	[SerializeField] public float DecelerationSpeed;
+	#endregion
 
+	#endregion
+
+	//= the hidden variabiles 
+	[HideInInspector] public float MomentumCounter; // whare you are inside one of the curves
+	[HideInInspector] public MovementStates CurrentState;
+	[HideInInspector] public float MoveX, MoveZ, VelocityScale;
 
 	void Start()
 	{
 		Rb = GetComponent<Rigidbody>();
 		CurrentState = new WalkState();
+
+		var startTime = Time.time;
+		var x = AccelerationCurve.Evaluate(Time.time);
+		if (Time.time - startTime <= 0)
+		{
+			Rb.velocity = MaxSpeed * x * Rb.velocity;
+		}
+		else
+		{
+			MomentumCounter += Time.deltaTime;
+		}
+
+		var duration = 4f;
+		x = AccelerationCurve.Evaluate(MomentumCounter);
+		if (MomentumCounter < duration)
+		{
+			MomentumCounter += Time.deltaTime;
+			Rb.velocity = MaxSpeed * x * Rb.velocity;
+
+
+			// if (no imput)
+			// {
+			// 	Exit();
+			// }
+		}
+		else
+		{
+			MomentumCounter = 0;
+		}
 	}
 
 	void Update()
--- Assets/Code/Scripts/Movement_Scripts/MovementStates.cs
+++ Assets/Code/Scripts/Movement_Scripts/MovementStates.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public abstract class MovementStates
@@ -13,6 +14,7 @@ public abstract class MovementStates
 
 public class WalkState : MovementStates
 {
+
 	public override void Enter(MovementController controller)
 	{
 
@@ -25,15 +27,55 @@ public class WalkState : MovementStates
 
 	public override void FixedTick(MovementController controller)
 	{
-
+		//controller.Rb.velocity *= controller.VelocityScale * controller.MaxSpeed* Time.DeltaTime* the Vector3 Movement;
 	}
 
 	public override void Tick(MovementController controller)
 	{
+		controller.VelocityScale = controller.AccelerationCurve.Evaluate(controller.MomentumCounter);
 
+		if (controller.MomentumCounter < 1)
+		{
+			controller.MomentumCounter += Time.deltaTime * controller.AccelerationSpeed;
+		}
+		else
+		{
+			controller.MomentumCounter = 1;
+		}
 	}
 }
 
+public class IdleState : MovementStates
+{
+	public override void Enter(MovementController controller)
+	{
+
+	}
+
+	public override void Exit(MovementController controller)
+	{
+
+	}
+
+	public override void FixedTick(MovementController controller)
+	{
+		//controller.Rb.velocity *= controller.VelocityScale * controller.MaxSpeed* Time.DeltaTime* the Vector3 Movement;
+	}
+
+	public override void Tick(MovementController controller)
+	{
+		controller.VelocityScale = controller.DecelerationCurve.Evaluate(controller.MomentumCounter);
+
+		if (controller.MomentumCounter > 0)
+		{
+			controller.MomentumCounter -= Time.deltaTime * controller.DecelerationSpeed;
+		}
+		else
+		{
+			controller.MomentumCounter = 0;
+		}
+	}
+}
 public class JumpState : MovementStates
 {
 	public override void Enter(MovementController controller)
