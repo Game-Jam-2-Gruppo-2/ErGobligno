110bd632ae0e0e62d5294576110fb3c9e04ad97f
FlavioNovati <f.novati10@gmail.com>
2024-04-29 17:49:18 +0200

    UI and Audio Manager Update

--- Assets/Code/Scripts/Audio_Scripts/AudioManager.cs
+++ Assets/Code/Scripts/Audio_Scripts/AudioManager.cs
@@ -1,7 +1,150 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] private AudioManager_Settings m_Settings;
+    [Header("Audio Settings")]
+    [SerializeField] private AudioMixer m_AudioMixer;
+    [SerializeField] private AudioManager_Settings m_AudioManager_Settings;
+    private List<AudioData> m_AudioData_List;
+    private static AudioMixer Mixer;
+
+    [Header("SFX Manager Settings")]
+    [SerializeField] private SFXManager_Settings m_SFX_Settings;
+    //SFX Events
+    public static Action<AudioClip, Vector3> Request3DSFX = (AudioClip clip, Vector3 pos) => { };
+    public static Action<AudioClip, Vector3> Request2DSFX = (AudioClip clip, Vector3 pos) => { };
+    //SFX Lists
+    private List<SFXEffect> Sources_3D = new List<SFXEffect>();
+    private List<SFXEffect> Sources_2D = new List<SFXEffect>();
+
+    private void Awake()
+    {
+        SetUpAudio();
+        SetUpSFX();
+        Request3DSFX += Request3D_SFX;
+        Request2DSFX += Request2D_SFX;
+    }
+    #region Audio Manager
+
+    private void SetUpAudio()
+    {
+        Mixer = m_AudioMixer;
+        m_AudioData_List = m_AudioManager_Settings.AudioData_List;
+        //Audio is saved in decimal scale an then converted
+        float volume = 0f;
+        for (int i = 0; i < m_AudioData_List.Count; i++)
+        {
+            volume = m_AudioData_List[i].DefaultVolume;
+            //Player Prefs
+            if (!PlayerPrefs.HasKey(m_AudioData_List[i].Type.ToString() + "Volume"))
+                PlayerPrefs.SetFloat(m_AudioData_List[i].Type.ToString() + "Volume", m_AudioData_List[i].DefaultVolume);
+            else
+                volume = PlayerPrefs.GetFloat(m_AudioData_List[i].Type.ToString() + "Volume");
+
+            //Convert audio volume
+            volume = 20 * Mathf.Log10(volume);
+            if (volume < -80f)
+                volume = -80f;
+            //Set Mixer volume
+            Mixer.SetFloat(m_AudioData_List[i].Type.ToString() + "Volume", volume);
+        }
+    }
+
+    /// <summary>
+    /// Change audio volume and save it into prefs
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="volume"></param>
+    public static void ChangeVolume(AudioChannel type, float volume)
+    {
+        //Update Player Prefs
+        PlayerPrefs.SetFloat(type.ToString() + "Volume", volume);
+
+        //Convert audio volume
+        volume = 20 * Mathf.Log10(volume);
+        if (volume < -80f)
+            volume = -80f;
+        //Set Mixer volume
+        Mixer.SetFloat(type.ToString() + "Volume", volume);
+    }
+    #endregion
+
+    #region SFX Manager
+    private void SetUpSFX()
+    {
+        //Instanciate 3D SFX
+        for (int i = 0; i < m_SFX_Settings.Max3DSFX; i++)
+        {
+            Sources_3D.Add(Instantiate(m_SFX_Settings.Source3D, transform));
+            Sources_3D[i].gameObject.SetActive(false);
+        }
+        //Instanciate 2D SFX
+        for (int i = 0; i < m_SFX_Settings.Max2DSFX; i++)
+        {
+            Sources_2D.Add(Instantiate(m_SFX_Settings.Source2D, transform));
+            Sources_2D[i].gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Place a game object containing a 3D audio source
+    /// </summary>
+    /// <param name="clip"></param>
+    /// <param name="position"></param>
+    public void Request3D_SFX(AudioClip clip, Vector3 position)
+    {
+        if (clip == null)
+            return;
+
+        for (int i = 0; i < m_SFX_Settings.Max3DSFX; i++)
+        {
+            if (!Sources_3D[i].gameObject.activeInHierarchy)
+            {
+                Sources_3D[i].gameObject.SetActive(true);
+                Sources_3D[i].PlaySound(position, clip);
+                return;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Place a game object containing a 2D audio source
+    /// </summary>
+    /// <param name="clip"></param>
+    /// <param name="position"></param>
+    public void Request2D_SFX(AudioClip clip, Vector3 position)
+    {
+        if (clip == null)
+            return;
+
+        for (int i = 0; i < m_SFX_Settings.Max2DSFX; i++)
+        {
+            if (!Sources_2D[i].gameObject.activeInHierarchy)
+            {
+                Sources_2D[i].gameObject.SetActive(true);
+                Sources_2D[i].PlaySound(position, clip);
+                return;
+            }
+        }
+    }
+    #endregion
+
+    private void OnDestroy()
+    {
+        Request3DSFX -= Request3DSFX;
+        Request2DSFX -= Request2DSFX;
+    }
+}
+
+/// <summary>
+/// Audio Enum Channels
+/// </summary>
+public enum AudioChannel
+{
+    Master,
+    Music,
+    SFX
 }
--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioManager), typeof(VFXManager), typeof(SFXManager))]
-public class Director : MonoBehaviour
+[RequireComponent(typeof(AudioManager), typeof(VFXManager))]
+public class GameManager : MonoBehaviour
 {
     //Singeton
-    [HideInInspector] public static Director Instance;
+    [HideInInspector] public static GameManager Instance;
+
+    /// <summary>
+    /// Game states
+    /// </summary>
+    private enum GameState
+    {
+
+    }
 
     private void Awake()
     {
